c942da3bd62806fc6983612a716343488ad9e862
Sasmitha2005 <156912853+Sasmitha2005@users.noreply.github.com>
2026-04-06 09:15:21 +0530

    Delete model3.py

--- model3.py
+++
@@ -1,8 +0,0 @@
-def contextual_analysis(parameters, age, gender):
-
-    for p in parameters:
-
-        if age > 50 and p["status"] == "Medium":
-            p["status"] = "High"
-
-    return parameters
